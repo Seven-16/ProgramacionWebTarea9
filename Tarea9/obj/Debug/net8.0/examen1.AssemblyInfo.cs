4d630634a4f1d91c870d1577977fb8b09b213357
Seven <aaronfdez09870@gmail.com>
2024-11-25 13:55:19 -0400

    Tarea9 Que Funciona

--- Tarea9/obj/Debug/net8.0/examen1.AssemblyInfo.cs
+++ Tarea9/obj/Debug/net8.0/examen1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("examen1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07f7d58827e4be46ea45165658e92b06447c7200")]
 [assembly: System.Reflection.AssemblyProductAttribute("examen1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("examen1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
